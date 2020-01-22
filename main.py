9f3a4ab823fd37809507cc3bb115bb67bcb1fe86
AbubakrHassan <abubakr.a.hassan@gmail.com>
2020-01-22 18:36:01 +0000

    moved to gpu

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def matplotlib_imshow(img, one_channel=False):
     if one_channel:
         img = img.mean(dim=0)
     img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
+    npimg = img.cpu().numpy()
     if one_channel:
         plt.imshow(npimg, cmap="Greys")
     else:
